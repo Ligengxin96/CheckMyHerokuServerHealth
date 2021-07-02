73b404fb305ad9a92c03fb50ef53dd5ae460dcad
Ligengxin <Ligengxin96@gmail.com>
2021-07-02 10:42:03 +0800

    Update request.js

--- utils/request.js
+++ utils/request.js
@@ -3,14 +3,14 @@ import axios from 'axios';
 const request = async(url, retryCount = 16) => {
   try {
     let tryCount = 0;
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
       const timer = setInterval(async() => {
         try {
           const response = await axios.get(url);
           console.log(`${new Date()}: ${tryCount + 1} times request ${url}`);
           if (++tryCount === retryCount) {
             clearInterval(timer);
-            throw new Error('No response from server, please check your server health.');
+            reject(new Error('No response from server, please check your server health.'));
           }
           clearInterval(timer);
           resolve(response.data);
@@ -20,7 +20,7 @@ const request = async(url, retryCount = 16) => {
       }, (tryCount + 1)  * 1 * 1000);
     });
   } catch (error) {
-    console.log(`${new Date()} error occurence, error: ${JSON.stringify(error)}`);
+    console.log(`${new Date()}: error occurence, error: ${JSON.stringify(error)}`);
     clearInterval(timer);
     throw error;
   }
