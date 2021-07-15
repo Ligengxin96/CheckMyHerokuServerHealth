3231fe95495ca48468cc1bd4ba36e169fbe485c0
Ligengxin <Ligengxin96@gmail.com>
2021-07-15 19:22:51 +0800

    bug fix

--- utils/request.js
+++ utils/request.js
@@ -1,28 +1,29 @@
 import axios from 'axios';
 
-const request = async(url, retryCount = 16) => {
+const request = async(url, retryCount = 5, interval = 30) => {
+  let tryCount = 0;
   try {
-    let tryCount = 0;
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
     return new Promise((resolve, reject) => {
       const timer = setInterval(async() => {
         try {
           const response = await axios.get(url);
-          console.log(`${new Date()}: the ${tryCount + 1} times request ${url}`);
-          if (++tryCount === retryCount) {
-            clearInterval(timer);
-            reject(new Error('No response from server, please check your server health.'));
-          }
+          console.log(`${new Date()}: the ${tryCount + 1} times retry request ${url} successfully.`);
           clearInterval(timer);
           resolve(response.data);
         } catch (error) {
-          console.log(`${new Date()}: the ${tryCount + 1} times request ${url} failed with error: ${error.message}`);
+          console.error(`${new Date()}: the ${tryCount + 1} times retry request ${url} failed with error: ${error.message}`);
+        } finally {
+          ++tryCount;
+          if (tryCount === retryCount) {
+            clearInterval(timer);
+            reject(new Error('No response from server, please check your server health.'));
+          }
         }
-      }, (tryCount + 1)  * 1 * 1000);
+      }, interval * 1000);
     });
-  } catch (error) {
-    console.log(`${new Date()}: error occurence, error: ${JSON.stringify(error)}`);
-    clearInterval(timer);
-    throw error;
   }
 }
 
