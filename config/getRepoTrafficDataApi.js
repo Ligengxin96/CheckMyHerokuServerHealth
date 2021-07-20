82e4c29ef34c903fb447155823bf16a4c3c5ed4b
Ligengxin <Ligengxin96@gmail.com>
2021-07-20 23:48:29 +0800

    Update getRepoTrafficDataApi.js

--- config/getRepoTrafficDataApi.js
+++ config/getRepoTrafficDataApi.js
@@ -1,4 +1,4 @@
 export default [
-  'http://www.ligengxin-server.top/getrepoinfo/v1/getrepoinfo/FetchRepoTrafficData/1',
-  'http://www.ligengxin-server.top/getrepoinfo/v1/getrepoinfo/FetchRepoTrafficData/1?aggregate=true&sort=asc'
+  'https://www.ligengxin-server.top/getrepoinfo/v1/getrepoinfo/FetchRepoTrafficData/1',
+  'https://www.ligengxin-server.top/getrepoinfo/v1/getrepoinfo/FetchRepoTrafficData/1?aggregate=true&sort=asc'
 ]
